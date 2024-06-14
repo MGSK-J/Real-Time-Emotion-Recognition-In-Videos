8764e378d5cd8f7d9801febbb3d56e38cb08ab91
MGSK-J <sonalikalpani@outlook.com>
2024-06-14 07:22:07 +0530

    src file
    folder added

---
+++ model/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="path_to_your_logo" alt="Mood Mirror Logo" />
+      </div>
+      <div className="history-link">
+        <a href="/history">Our History</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
